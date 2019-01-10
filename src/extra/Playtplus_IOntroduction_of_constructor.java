759acedc4ed6a5bcb3619259ade5adc06bf285f3
league <league@wts9.attlocal.net>
2019-01-09 18:27:58 -0800

    introductiontoconstrucotr

---
+++ src/extra/Playtplus_IOntroduction_of_constructor.java
@@ -0,0 +1,12 @@
+package extra;
+
+public class Playtplus_IOntroduction_of_constructor {
+private  String name;
+void sayHi() {
+	System.out.println("The Platypus"+name+"is smarter than your average playtplus.");
+	
+}
+Playtplus_IOntroduction_of_constructor(String name){
+this.name=name;
+}
+}
---
+++ src/extra/runner_Introduction_to_constructor.java
@@ -0,0 +1,8 @@
+package extra;
+
+public class runner_Introduction_to_constructor {
+	public static void main(String[] args) {
+		Playtplus_IOntroduction_of_constructor p= new Playtplus_IOntroduction_of_constructor("pp");
+		p.sayHi();
+	}	
+}
